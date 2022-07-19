c97e4b7d7499b658c3b7a1d4b62878e919e21cc3
Carly Donais <carlydonais@Carlys-MBP.lan>
2022-07-19 14:48:48 -0400

    created user seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+const userData=[
+    {
+        username: 'techgirl',
+        password: 'girlcode'
+    },
+    {
+        username: 'user2',
+        password: 'secretpass'
+    },
+    {
+        username: 'luv2code',
+        password: 'passpass'
+    },
+    {
+        username: 'cooluser',
+        password: 'mypassword'
+    }
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;

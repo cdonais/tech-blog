89b3ab588f2ac80a38c27915188415a6fec141c4
Carly Donais <carlydonais@Carlys-MBP.lan>
2022-07-19 14:56:25 -0400

    added seed comments

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,28 @@
+const { Comment } = require('../models');
+
+const commentData = [
+    {
+        comment_text: "This is a good post",
+        user_id: 1,
+        post_id: 1
+    },
+    {
+        comment_text: "This is a bad post",
+        user_id: 2,
+        post_id: 1
+    },
+    {
+        comment_text: "I like it",
+        user_id: 3,
+        post_id: 2
+    },
+    {
+        comment_text: "Cool",
+        user_id: 4,
+        post_id: 3
+    }
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;

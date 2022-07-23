16178be7d77351ba4614ab4265f571523d23b223
Carly Donais <carlydonais@Carlys-MacBook-Pro.local>
2022-07-23 16:34:52 -0700

    added sign-up route

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -38,4 +38,9 @@ router.get('/', (req, res) => {
   
         res.render('login');
     })
+
+    router.get('/sign-up', (req, res) => {
+ 
+      res.render('sign-up');
+    });
 module.exports = router;
